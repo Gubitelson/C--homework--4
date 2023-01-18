9c53562f80f62068d3233679ba18fd14645b94e7
Gubitelson <119538200+Gubitelson@users.noreply.github.com>
2023-01-18 19:38:17 +0400

    homework

---
+++ №1/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Домашнее задание №1. 
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+Console.Write("Введите 1-е число = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите 2-е число = ");
+int b = int.Parse(Console.ReadLine());
+
+void Multiplicate(int a, int b)
+{
+    int mult = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        mult *= a;
+    }
+    Console.Write(mult);
+}
+Multiplicate(a, b);
---
+++ №3/Program.cs
@@ -0,0 +1,11 @@
+﻿/*  Домашнее задание №3. 
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+ */
+Console.Clear();
+
+int [] mass = new int[8];
+for(int i = 0; i < mass.Length; i++)
+{
+    mass [i] = new Random().Next(9999);
+} 
+Console.WriteLine($"Массив {String.Join(", ", mass)}");
